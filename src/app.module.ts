4aef555fc8fe973bfa68f3c3653b3cf79a2f8131
dontroll17 <dontroll@yandex.ru>
2021-12-01 14:16:17 +0300

    sitemap controller and service. http://localhost:3333/api/sitemap/xml return sitemap xml

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { getMongoConfig } from './configs/mongo.config';
 import { FilesModule } from './files/files.module';
+import { SitemapModule } from './sitemap/sitemap.module';
 
 @Module({
   imports: [
@@ -21,7 +22,7 @@ import { FilesModule } from './files/files.module';
     AuthModule,
     TopPageModule, 
     ProductModule, 
-    ReviewModule, FilesModule
+    ReviewModule, FilesModule, SitemapModule
   ],
   controllers: [AppController],
   providers: [AppService],
